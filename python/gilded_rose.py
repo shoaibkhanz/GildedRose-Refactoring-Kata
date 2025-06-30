88163cdcda6c9a059effabfff1faef39f5e7fd67
shoaibkhanz <shoaibkhanz@hotmail.com>
2025-07-01 00:01:23 +0100

    added some factory items
    factory Items now incorporates their logic within them, I believe we
    could organise this further into directories but I will avoid it for
    this refactoring exercise, since it may be an overkill.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -21,6 +21,53 @@ class FactoryItems(ABC):
         pass
 
 
+class NormalItem(FactoryItems):
+    def update_quality(self) -> None:
+        """
+        Represents a normal item that degrades in quality over time.
+
+        - `sell_in` decreases by 1 each day.
+        - `quality` decreases by 1 per day.
+        - Once the `sell_in` date has passed (`sell_in < 0`), `quality` decreases twice as fast (2 per day).
+        - `quality` is never negative.
+        - If initialized with `quality > 50`, it will be capped at 50 during the next update to enforce the quality ceiling.
+
+        """
+        self.items.sell_in -= 1
+        degrade_item = 2 if self.items.sell_in < 0 else 1
+        self.items.quality = max(0, self.items.quality - degrade_item)
+        if self.items.quality > 50:
+            self.items.quality = 50
+
+
+class AgedBrieItem(FactoryItems):
+    def update_quality(self) -> None:
+        """
+        Aged Brie is a special item that increases in quality the older it gets.
+
+        - Each day, its `sell_in` decreases by 1.
+        - Its `quality` increases by 1 per day.
+        - If `quality` is below 1, it gets an additional increase (though this may not be necessary based on rules).
+        - Quality is capped at 50, and the logic ensures it never exceeds this limit.
+        """
+        self.items.sell_in -= 1
+        self.items.quality += 1
+        if self.items.quality < 1:
+            self.items.quality += 1
+        elif self.items.quality > 50:
+            # NOTE: I could decrement the value by 1, however, this is to
+            # avoid a situation where we manually assigned the value greater
+            # than 51, for e.g. 52 then a decrementing would make it by 1
+            # would make it 51 and not 50.
+            self.items.quality = 50
+
+
+class Sulfuras(FactoryItems):
+    def update_quality(self) -> None:
+        """Sulfuras is legendary: no changes to sell_in or quality."""
+        pass
+
+
 class GildedRose(object):
     def __init__(self, items):
         self.items = items
